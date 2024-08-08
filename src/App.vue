67821afca8ed30fc7cf09a9f1af3180b303bb212
cssmagic <cssmagic.cn@gmail.com>
2024-08-09 01:31:19 +0800

    feat: [10.8] 功能增强：自定义配置

--- src/App.vue
+++ src/App.vue
@@ -7,16 +7,22 @@
 <script setup>
 import { ref, nextTick, onMounted } from 'vue'
 import MessageItem from '@/components/MessageItem.vue'
+import ConfigDialog from '@/components/ConfigDialog.vue'
 import { mockDataMessages } from '@/utils/mock.js'
 import { getMockResponse, getResponse } from '@/utils/message.js'
+import { hasValidConfig } from '@/utils/config.js'
 
-const $messages = ref(mockDataMessages)
+const $messages = ref([])
 const $messageList = ref(null)
 
 const $inputContent = ref('')
 const $inputElement = ref(null)
 
 const $isLoading = ref(false)
+const $shouldShowDialog = ref(false)
+
+// 这个变量用来保存配置完整性的检查结果
+const $hasValidConfig = ref(hasValidConfig())
 
 function scrollToBottom() {
 	// 消息列表滚动到底部，需要平滑滚动
@@ -24,6 +30,12 @@ function scrollToBottom() {
 		top: $messageList.value.scrollHeight,
 		behavior: 'smooth',
 	})
+}
+
+function onCloseDialog() {
+	$shouldShowDialog.value = false
+	// 当配置弹框关闭时，重新检查配置完整性
+	$hasValidConfig.value = hasValidConfig()
 }
 
 async function onSubmit() {
@@ -98,20 +110,46 @@ onMounted(() => {
 		它的高度撑满整个页面，最大宽度为 640px，并且居中显示。
 		我们一步一步来，暂时不需要考虑它的内容。
 	-->
-	<div class="mx-auto max-w-[640px] h-full overflow-hidden bg-white flex flex-col">
+	<div class="mx-auto max-w-[640px] h-full overflow-hidden bg-white flex flex-col relative">
 		<!--
 			这里需要仿手机 App 的聊天界面布局形式：
 			* 顶部是标题栏，高度固定。
 			* 底部是操作栏，高度固定。
 			* 中间区域是对话区，高度占据剩余空间，当内容超长时可以出现垂直滚动条。
 		-->
-		<header class="h-11 bg-gray-100 border-b flex-none flex items-center justify-center">
+		<header class="h-11 bg-gray-100 border-b flex-none flex items-center justify-center relative">
 			<h1 class="font-bold text-lg">Simple Chat</h1>
+			<div class="absolute top-0 right-0 h-full flex items-center justify-center">
+				<button
+					class="px-5 h-full flex items-center justify-center text-blue-500 hover:text-blue-600"
+					@click="$shouldShowDialog = true"
+				>配置</button>
+			</div>
 		</header>
 		<div ref="$messageList" class="p-5 pb-10 flex-auto overflow-y-auto">
-			<MessageItem v-for="message of $messages" :messageItem="message"></MessageItem>
+			<div v-if="! $hasValidConfig">
+				<div class="text-gray-400 text-center mt-5">
+					（您还没有配置模型）
+				</div>
+				<button
+					class="block mt-5 mx-auto py-3 w-[6em] text-blue-500 rounded border hover:text-blue-600 hover:border-blue-500"
+					@click="$shouldShowDialog = true"
+				>点此配置</button>
+			</div>
+			<div v-else-if="$messages.length === 0" class="text-gray-400 text-center mt-5">
+				（您还没有发过消息）
+			</div>
+			<template v-else>
+				<MessageItem
+					v-for="message of $messages"
+					:messageItem="message"
+				></MessageItem>
+			</template>
 		</div>
-		<footer class="h-12 border-t flex-none flex items-stretch justify-between">
+		<footer
+			class="h-12 border-t flex-none flex items-stretch justify-between"
+			v-if="$hasValidConfig"
+		>
 			<div class="flex-auto">
 				<input
 					class="px-4 w-full h-full bg-white outline-0 border-transparent border-2 focus:border-blue-300"
@@ -130,6 +168,11 @@ onMounted(() => {
 				>发送</button>
 			</div>
 		</footer>
+
+		<ConfigDialog
+			v-if="$shouldShowDialog"
+			@close="onCloseDialog"
+		></ConfigDialog>
 	</div>
 </template>
 
